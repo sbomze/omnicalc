77a6067cfb1a987d3f77ba922d2e605462c9ca91
Steve Bomze <sbomze@chicagobooth.edu>
2017-04-14 19:44:41 -0500

    v5

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -107,7 +107,7 @@ class CalculationsController < ApplicationController
 
     @standard_deviation = @variance**0.5
 
-    @mode = a.group_by(&:itself).values.max_by(&:size).first
+    @mode = @numbers.group_by(&:itself).values.max_by(&:size).first
     # @numbers.max_by { |v| @numbers.inject(Hash.new(0)) { |h,v| h[v] += 1; h }}
 
     # ================================================================================
